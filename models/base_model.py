e287bc8123e752e4b2f35b8dcc4b2a3e8559c45d
Yirga Mulaw <infoyirga12@gmail.com>
2023-02-10 13:37:58 +0300

    Base Model

---
+++ models/base_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This script is the base model"""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+
+    """Class from which all other classes will inherit"""
+    
+    __init__(self, created_at, updated_at):
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+        def __str__(self):
+        """Returns official string representation"""
+
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """updates the public instance attribute updated_at"""
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__"""
+
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict

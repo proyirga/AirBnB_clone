d63fd8592ffc63760dad03b90a7251cad208f857
Noah Tsegay <noahtsegay8@gmail.com>
2023-02-12 15:41:20 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,7 @@ class BaseModel:
                     self.created_at = datetime.strptime(kwargs["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
                 if "updated_at" in kwargs:
                     self.updated_at = datetime.strptime(kwargs["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
-            else:
+            return
                 self.id = str(uuid.uuid4())
                 self.created_at = datetime.now()
                 self.updated_at = datetime.now()

5d3a3236350bbb5832f4a140261a2d785044f8bd
Noah Tsegay <noahtsegay8@gmail.com>
2023-02-12 15:13:15 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@
 import models
 import uuid
 from datetime import datetime
-import json
+
 
 
 class BaseModel:
@@ -13,16 +13,20 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """ instantiates an object with its attributes """
 
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            return
-
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+        def __init__(self, *args, **kwargs):
+            if kwargs:
+                for key, value in kwargs.items():
+                    if key != "__class__":
+                        setattr(self, key, value)
+                if "created_at" in kwargs:
+                    self.created_at = datetime.strptime(kwargs["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
+                if "updated_at" in kwargs:
+                    self.updated_at = datetime.strptime(kwargs["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+                models.storage.new(self)
 
     def __str__(self):
         """Returns official string representation"""
@@ -33,8 +37,6 @@ class BaseModel:
         """updates the public instance attribute updated_at"""
 
         self.updated_at = datetime.now()
-        with open("file.json", "w") as file:
-            json.dump(self.to_dict(), file)
         models.storage.save()
 
     def to_dict(self):

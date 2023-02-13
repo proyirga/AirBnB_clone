ad74636d779350368b23675860acc5137a5e171e
Yirga Mulaw <infoyirga12@gmail.com>
2023-02-13 09:28:37 +0300

    FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -36,6 +36,25 @@ class FileStorage:
         with open(FileStorage.__file_path, "w", encoding="UTF-8") as f:
             f.write(json.dumps(my_dict))
 
+    def classes(self):
+        """Returns a dictionary of valid classes and their references"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
+
     def reload(self):
         """
         deserializes the JSON file to __objects only if the JSON
